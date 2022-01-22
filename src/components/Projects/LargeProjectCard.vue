e00dc9e40214789fb91f0b15bb9e87fe0ae5ad22
Killian Mahé <killianmahe.pro@gmail.com>
2022-01-22 09:40:20 -0500

    :sparkles: Add project links

--- src/components/Projects/LargeProjectCard.vue
+++ src/components/Projects/LargeProjectCard.vue
@@ -4,10 +4,26 @@
 
 
   <div>
-    <h2 class="text-primary font-mono text-3xl font-bold">{{ project.name }}</h2>
+    <div class="flex justify-between items-center">
+      <h2 class="text-primary font-mono text-3xl font-bold">
+        {{ project.name }}
+      </h2>
 
-    <div class="flex justify-between mb-10">
-      <p class="font-sans font-light">{{ project.description }}</p>
+      <div class="flex space-x-2">
+        <a v-if="project.links.github" target="_blank" :href="project.links.github">
+          <svg class="project-links" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+        </a>
+        <a @click.prevent="openFile(project)" v-if="project.links.download">
+          <svg class="project-links" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>
+        </a>
+        <a v-if="project.links.demo" :href="project.links.demo" target="_blank">
+          <svg class="project-links" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>
+        </a>
+      </div>
+    </div>
+
+    <div class="flex justify-between mb-5 mt-2">
+      <p class="font-sans">{{ project.description }}</p>
       <span class="flex space-x-2"><Tag class="h-fit" v-for="tag in project.tags" :outline="true">{{ tag }}</Tag></span>
     </div>
 
@@ -34,7 +50,7 @@ import Carousel from "../Carousel.vue"
 import Tag from "./Tag.vue";
 
 export default {
-  name: "ProjectCard.vue",
+  name: "LargeProjectCard.vue",
   components: {
     Carousel,
     Tag
@@ -44,10 +60,18 @@ export default {
       type: Object,
       required: true
     }
+  },
+  methods: {
+    openFile(project) {
+      window.open(project.links.download)
+    }
   }
 }
 </script>
 
 <style scoped>
-
+.project-links {
+  @apply h-8 opacity-50 hover:opacity-100;
+  @apply transition duration-100 ease-in-out;
+}
 </style>

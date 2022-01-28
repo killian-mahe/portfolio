1325f0af82585f4f4b3e3f4c03db7dba16a015b9
Killian Mahé <killianmahe.pro@gmail.com>
2022-01-28 09:54:20 -0500

    :sparkles: Finish desktop portfolio

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,32 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import NavBar from './components/NavBar.vue'
+import GithubLogo from './assets/github.svg'
 </script>
 
 <template>
-  <div class="lg:grid lg:grid-cols-8">
-    <div></div>
-    <div class="lg:col-span-6">
-      <NavBar/>
+  <div class="min-h-[100vh] flex flex-col justify-items-stretch">
+    <div class="lg:grid lg:grid-cols-8">
+      <div></div>
+      <div class="lg:col-span-6">
+        <NavBar/>
+      </div>
+      <div></div>
     </div>
-    <div></div>
-  </div>
 
-  <router-view/>
+    <router-view/>
+
+    <div class="flex justify-self-end justify-center space-x-8 mb-10 mt-10">
+      <a href="https://github.com/killian-mahe" target="_blank">
+        <svg class="hover:stroke-primary transition ease-in-out duration-150 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+      </a>
+      <a href="https://www.linkedin.com/in/killian-mah%C3%A9-246928135/" target="_blank">
+        <svg class="hover:stroke-primary transition ease-in-out duration-150 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+      </a>
+      <a href="mailto:killianmahe.pro@gmail.com">
+        <svg class="hover:stroke-primary transition ease-in-out duration-150 cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+      </a>
+    </div>
+  </div>
 
 </template>
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,13 +1,52 @@
 <template>
+  <Title class="my-12"/>
 
+  <form class="mx-auto my-24 w-1/2" @click.prevent="onClick">
+    <div class="flex space-x-4">
+      <div class="w-1/3">
+        <label for="name_field">Your name</label>
+        <input id="name_field" class="w-full h-10" type="text" placeholder="Killian Mahé"/>
+      </div>
+      <div class="w-1/3">
+        <label for="email_field">Your email</label>
+        <input id="email_field" class="w-full h-10" type="email" placeholder="killianmahe.pro@gmail.com"/>
+      </div>
+      <div class="w-1/3">
+        <label for="text_field">Subject</label>
+        <input id="text_field" class="w-full h-10" type="text" placeholder="Subject"/>
+      </div>
+    </div>
+    <textarea class="w-full mt-8" rows="5" placeholder="Your mail content"></textarea>
+    <Button class="w-32 h-12 mx-auto mt-8">Send</Button>
+  </form>
 </template>
 
 <script>
+import Title from '../components/Contact/Title.vue'
+import Button from '../components/Button.vue'
+
 export default {
-  name: "Contact.vue"
+  name: "Contact.vue",
+  components: {
+    Title,
+    Button
+  },
+  methods: {
+    onClick() {
+
+    }
+  }
 }
 </script>
 
 <style scoped>
+input {
+  @apply border-2 border-black outline-0;
+  @apply focus:border-primary p-2;
+}
 
+textarea {
+  @apply border-2 border-black outline-0;
+  @apply focus:border-primary p-2;
+}
 </style>
